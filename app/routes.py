06c0a845ad2b0bb4fe754f650b4d2ec614b694cc
IleanaP <ileana.profeanu@gmail.com>
2019-09-03 11:15:07 +0300

    added 404 page

--- app/routes.py
+++ app/routes.py
@@ -9,13 +9,6 @@ from flask import render_template, flash, redirect, url_for, request
 from app import app
 from app.forms import UploadForm
 
-def isFileExtentionAllowed(filename):
-    ext = filename.split('.')[1].lower()
-    if ext in app.config['ALLOWED_EXTENSIONS']:
-        return 1
-    
-    return 0
-
 @app.route('/')
 @app.route('/index')
 def index():
@@ -29,9 +22,10 @@ def example():
 def upload():
     form = UploadForm()
     if request.method == 'POST':
-#        if 'file' not in request.files:
-#            flash('No file was uploaded. Please try again - from routes')
-#        else:
         form.tryUploadFileToServer()
-            
     return render_template('upload.html', title="Upload Form", form=form)
+
+""" ERROR HANDLING  """
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html', title="404 Not Found")
