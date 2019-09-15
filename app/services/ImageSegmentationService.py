2f09ef369438c7a35a96d7ad8d63220c615b4fe5
IleanaP <ileana.profeanu@gmail.com>
2019-09-15 19:15:58 +0300

    some other changes - unfinished

--- app/services/ImageSegmentationService.py
+++ app/services/ImageSegmentationService.py
@@ -32,14 +32,21 @@ class Region:
         self.h = 0
         self.w = 0
         self.ratio = 0
-        
-        self.bigOutlier = False   # used to determine wether a region is too big, or has more letters
-        self.smallOutlier = False # same as above, only for small regions
-        self.ratioBigOutlier = False
-        self.ratioSmallOutlier = False
-        self.isDot = False
-        
+        self.density = 0
         self.area = 0
+        
+        self.line = -1
+        
+#        self.splitVal = 0
+#        self.isNormalRatioOver = False
+#        self.isNormalRatioUnder = False
+        
+        self.out = {
+                    "areaOver"    : False, "areaUnder"    : False, # used to determine wether a region is too big, or has more letters
+                    "ratioOver"   : False, "ratioUnder"   : False, # same as above, only for small regions
+                    "densityOver" : False, "densityUnder" : False, 
+                    "dot"         : False
+                   }
         
         self.N = h #smallest row in the list of points
         self.S = 0 #biggestt row
@@ -70,9 +77,23 @@ class Region:
         self.h = self.S - self.N + 1
         self.w = self.E - self.W + 1
         
+#        if self.area == 501:
+#            print("willl calculate ratio")
+#            print("~~~~~~~")
+#            print("width: ", self.w)
+#            print("height: ", self.h)
+#            self.ratio = float(self.w)/float(self.h)
+#            print("unexpected result: ", self.ratio)
         self.ratio = float(self.w)/float(self.h)
         
         return self.ratio
+    
+    def calculateDensity(self):
+        if self.area != 0:
+            totalArea = (self.S - self.N) * (self.E - self.W)
+            self.density = self.area/totalArea
+            
+        return self.density
     
     def join(self, reg):
         self.area = self.area + reg.area
@@ -81,6 +102,24 @@ class Region:
         self.S = max(self.S, reg.S)
         self.W = min(self.W, reg.W)
         self.E = max(self.E, reg.E)
+        
+    def wipe(self):
+        self.area = 0
+        
+        self.N, self.S, self.W, self.E = 0, 0, 0, 0
+        
+    def setOutliers(self, area, ratio, density):
+        
+        self.calculateRatio()
+        
+        self.out['areaOver']     = True if self.area    > area['m']    + area['sd']    else False
+        self.out['areaUnder']    = True if self.area    < area['m']    - area['sd']    else False
+        self.out['ratioOver']    = True if self.ratio   > ratio['m']   + ratio['sd']   else False
+        self.out['ratioUnder']   = True if self.ratio   < ratio['m']   - ratio['sd']   else False
+        self.out['densityOver']  = True if self.density > density['m'] + density['sd'] else False
+        self.out['densityUnder'] = True if self.density < density['m'] - density['sd'] else False
+
+        self.out['dot'] = True if (self.ratio >= 0.7 and self.ratio <= 1.3 and self.out['areaUnder']) else False
 
     def cropImg(self, mask):        
         self.cropped = mask[self.N:self.S+1, self.W:self.E+1]
@@ -91,21 +130,21 @@ class Region:
         
         fileName = str(self.area) + '_' + str(self.label)
         
-#        if self.bigOutlier:
-#            fileName = fileName + "_AreaOutlier"
-#        if self.ratioBigOutlier:
-#            fileName = fileName + "_RatioBigOutlier"
-#        if self.smallOutlier:
-#            fileName = fileName + "_AreaOutlier"            
-#        if self.ratioSmallOutlier:
-#            fileName = fileName + "_RatioSmallOutlier"
+#        if self.out["areaUnder"] or self.out["areaOver"]:
+#            fileName = fileName + "_AO"
+#        if self.out["ratioOver"]:
+#            fileName = fileName + "_RO"
+#        if self.out["ratioUnder"]:
+#            fileName = fileName + "_RU"
+#        if self.out["densityOver"]:
+#            fileName = fileName + "_DO"
+#        if self.out["densityUnder"]:
+#            fileName = fileName + "_DU"
 #        if self.isMargin():
-#            fileName = fileName + "_margin"
-            
-        if self.isDot:
-            fileName = fileName + "_dot"
+#            fileName = fileName + "_M"
                 
-        fileName = fileName + "N" + str(self.N) + "S" + str(self.S) + "W" + str(self.W) + "E" + str(self.E) + ".png"
+#        fileName = fileName + "N" + str(self.N) + "S" + str(self.S) + "W" + str(self.W) + "E" + str(self.E) + ".png"
+        fileName = fileName + ".png"
         
 #        if self.area == 235:
 #            print("N: ", self.N)
@@ -113,6 +152,11 @@ class Region:
 #            print("W: ", self.W)
 #            print("E: ", self.E)
 #            sys.exit()
+        
+        if self.area == 501:
+            print("area 51 RATIO: ", self.ratio)
+            print("area 51 WIDTH: ", self.w)
+            print("area 51 HEIGHT: ", self.h)
         
         scipy.misc.imsave('cropped/' + fileName, self.cropped)
 #        print(str(self.area) + '_' + str(self.label) + '.png', end=', ')
@@ -189,96 +233,154 @@ class CCL:
         
         self.labels = [[self.addPoint(i,j) 
                         for j in range(self.w) if self.labels[i][j] != -1] for i in range(self.h)]
-        
-#        self.regions = [reg for reg in self.regions if reg.area > 0]
+
         self.regions = [reg for reg in self.regions if reg.isEligible()]
         
         timeElapsed = time.time() - timeElapsed
         print('Relabel Points - ', timeElapsed)
         
     def findOutliers(self):        
-        # vreau sa scot margins, si regions cu aria 1
-        # vreau sa gasesc outliers in functie de area (standard deviation)
-        # dintre outlierii mici, vreau sa gasesc semne de punctuatie, puncte pe i/j, etc
+        # vreau sa scot margins, si regions cu aria 1 - OK
+        # vreau sa gasesc outliers in functie de area (standard deviation) - OK
+        # dintre outlierii mici, vreau sa gasesc semne de punctuatie, puncte pe i/j, etc - OK
         # dintre outlierii mari, vreau sa gasesc daca sunt mai multe litere in ei, si sa ii sparg
         # --- in functie de ratio a elementelor care nu sunt outlieri (SD a ratio)
         # daca dintre outlierii mari sunt linii orizontale __ sau verticale | care nu sunt margini? 
         # --- scot liniile | si liniile __ devin "_ _ _ _" in textul final (SD a ratio)
         
-        for reg in self.regions:
-            reg.calculateRatio()
+        # sa despart literele folosindu-ma de NRO sau de width a literelor de pe aceeasi linie
+        # sa leg punctul de la "!" (daca e cazul)
+        # sa gasesc punct si virgula in functie de density :D
         
-        regionAreas = [reg.area for reg in self.regions]
-        
-        areaMean, areaStDev = self.getMeanStDev(regionAreas)        
-        print('area mean: ', areaMean)
-        print('area st dev: ', areaStDev)
+        self.setMetric()
         
         for reg in self.regions:
-            if reg.area > areaMean + areaStDev:
-                reg.bigOutlier = True
-            if reg.area < areaMean - areaStDev:
-                reg.smallOutlier = True
+            if reg.out["dot"]:
+                self.tryFindRegionParent(reg)
+        
+        self.regions = [reg for reg in self.regions if reg.isEligible()]
+        
+        self.setMetric()
+#        self.setSomeRatio()
+        
+        # pentru a sparge regions cu mai multe litere in literele separate:
+        # -- gasesc liniile, apoi gasesc mean height pe linie
+        # -- gasesc mean width in functie de Mean Normal Ratio a elems care nu sunt symbols (setSomeRatio), si mean height:
+        #    mW = mNR * mH
+        # (ar fi bine ca din preprocesare liniile sa nu fie sloped - AR FI XD)
+        self.assignLines()
+        
+    def assignLines(self):
+        currLine = 0
+        
+        for reg in self.regions:
+            # gasesc clase de echivalenta de neighbors pe orizontala
+            # regions cu aceeasi clasa de echivalenta de neighbors sunt pe aceeasi linie
+            neighbors = self.getNeighborRegions(reg)
+            
+            neighborLines = [neighbor.line for neighbor in neighbors if neighbor.line != -1]
+            
+            if len(neighborLines) == 0:
+                for neighbor in neighbors:
+                    neighbor.line = currLine
+                reg.line = currLine
+                currLine = currLine + 1
+            else:
+                reg.line = min(neighborLines)
+                for neighbor in neighbors:
+                    neighbor.line = reg.line
 
-        regionRatios = [reg.calculateRatio() for reg in self.regions]
+    def getNeighborRegions(self, reg):
+        # TO DO - neighbors pe orizontala a lui reg
+        neighbors = self.regions[0]
         
-        ratioMean, ratioStDev = self.getMeanStDev(regionRatios)        
-        print('ratio mean: ', ratioMean)
-        print('ratio st dev: ', ratioStDev)
+        return neighbors
         
-#        regLabels = [reg.label for reg in self.regions]
-#        print(regLabels)
-#        sys.exit()
         
-        self.regionsToRemove = []
-        self.regionsMerged = []
-        
-        for reg in self.regions:
-            if reg not in self.regionsToRemove and reg not in self.regionsMerged:            
-                if reg.ratio > ratioMean + ratioStDev:
-                    reg.ratioBigOutlier = True
-                if reg.ratio < ratioMean - ratioStDev:
-                    reg.ratioSmallOutlier = True
-                if reg.ratio >= 0.7 and reg.ratio <= 1.3 and reg.smallOutlier:
-                    reg.isDot = True
-                if reg.isDot:
-                    self.tryFindRegionParent(reg)
-        
-#        for reg in self.regionsToRemove:
-#            self.regions.remove(reg)
+    def removeSlope():
+        minN = min([reg.N for reg in self.regions])
+        maxS = max([reg.S for reg in self.regions])
+        minW = min([reg.W for reg in self.regions])
+        maxE = max([reg.E for reg in self.regions])
         
     def tryFindRegionParent(self, reg):
-        if reg.isDot:
+        if reg.out["dot"]:
             areaH = reg.h * 4
             
-            tempCropped = self.mask[reg.N + reg.h:reg.N+areaH+1, reg.W:reg.E+1]                
-                
-            tempCroppedArray = np.unique(np.asarray(tempCropped))
+            # get 3h*w rectangle that starts 1h below dot "reg" region
+            cropped = self.mask[reg.N + reg.h:reg.N+areaH+1, reg.W:reg.E+1]                
             
-            print("###")
-            print(tempCroppedArray)
-            print(tempCroppedArray.shape)
-            print("###")
-                  
-            tempCroppedArray = [tmp for tmp in tempCroppedArray if tmp != -1]
+            # find unique labels corresponding to above region
+            croppedLabels = np.unique(np.asarray(cropped))
             
-            if len(tempCroppedArray) == 1:
-                desiredLabel = tempCroppedArray[0]
-                print("~~", desiredLabel, "~~")
-                desiredRegion = [x for x in self.regions if x.label == desiredLabel][0]
-#                desiredRegion = desiredRegion
+            # remove default label (-1)
+            croppedLabels = [tmp for tmp in croppedLabels if tmp != -1]
+            
+            # if 1 unique label was found, it is a good "parent" candidate for the dot, we will join the regions
+            # if more than one or none is found, the result is inconclusive, do nothing
+            if len(croppedLabels) == 1:
+                croppedLabel = croppedLabels[0]
+                desiredRegion = [x for x in self.regions if x.label == croppedLabel][0]
                     
-                self.mask[self.mask == reg.label] = desiredRegion.label
+                self.mask[self.mask == reg.label] = croppedLabel
                 desiredRegion.join(reg)
-                    
-                self.regionsToRemove.append(reg)
-                self.regionsMerged.append(reg)
+                reg.wipe()
+                
+                desiredRegion.setOutliers(self.area, self.ratio, self.density)
+                reg.setOutliers(self.area, self.ratio, self.density)
+                
+    def assignLines(self):
+        
+        
+                
+#    def setSomeRatio(self):
+#        regionsTemp = [reg.ratio for reg in self.regions if reg.out["densityOver"] is not True]
+#
+#        tmp = self.getMeanStDev(regionsTemp)
+#        
+#        for reg in self.regions:
+#            reg.isNormalRatioOver = True if reg.ratio > tmp['m'] + tmp['sd'] else False
+#            reg.isNormalRatioUnder = True if reg.ratio < tmp['m'] - tmp['sd'] else False
+#            if reg.ratio > tmp['m'] + tmp['sd'] and reg.out["areaOver"]:
+#                meanW = tmp["m"] * reg.h
+#                if reg.area == 962:
+#                    print("meanW: ", meanW)
+#                splitValue = int(round(meanW))
+#                if reg.area == 375:
+#                    print("splitValue: ", splitValue)
+#                    print("reg w: ", reg.w)
+#                reg.splitVal =int(round(reg.w/splitValue))
+#                if reg.area == 375:
+#                    print("reg splitVal: ", reg.splitVal)
+#                    sys.exit()
+                
+    def setMetric(self):
+        regionAreas = [reg.area for reg in self.regions]
+        self.area = self.getMeanStDev(regionAreas) # get area mean (M) and area standard deviation (SD)
+        
+        regionRatios = [reg.calculateRatio() for reg in self.regions]
+        self.ratio = self.getMeanStDev(regionRatios)
+        
+        regionDensity = [reg.calculateDensity() for reg in self.regions]
+        self.density = self.getMeanStDev(regionDensity)
+        
+        for reg in self.regions:
+            reg.setOutliers(self.area, self.ratio, self.density)
+        
+        print("~~~~")
+        print("area Mean: ", self.area['m'])
+        print("area SD: ", self.area['sd'])
+        print("ratio Mean: ", self.ratio['m'])
+        print("ratio SD: ", self.ratio['sd'])
+        print("density Mean: ", self.density['m'])
+        print("density SD: ", self.density['sd'])
+        print("~~~~")
         
     def getMeanStDev(self, elems):
-        mean = int(sum(elems) / len(elems))
+        mean = sum(elems) / len(elems)
         stDev = statistics.pstdev(elems) # standard deviation
         
-        return mean, stDev
+        return { 'm': mean, 'sd': stDev }
         
     def addPoint(self, i, j):        
         currLabelHat = self.eqClassesHat[self.labels[i][j]]
@@ -292,4 +394,4 @@ class CCL:
         for region in self.regions:
             region.cropImg(self.mask)
                 
-thisCCL = CCL("ccl_image_03.jpg")
+thisCCL = CCL("ccl_image_02.jpg")
